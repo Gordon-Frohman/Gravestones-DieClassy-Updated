a65aa9b359471850997545a81a814263ff674e2a
Gordon-Frohman <sanisimov6699@gmail.com>
2024-12-14 21:46:41 +0700

    Update AetherIntegration.java

--- src/main/java/net/subaraki/gravestone/integration/AetherIntegration.java
+++ src/main/java/net/subaraki/gravestone/integration/AetherIntegration.java
@@ -13,16 +13,15 @@ import com.gildedgames.the_aether.items.ItemsAether;
 
 public class AetherIntegration {
 
-    private static AccessoryType[] slotTypes = new AccessoryType[] { AccessoryType.PENDANT, AccessoryType.CAPE,
-        AccessoryType.SHIELD, AccessoryType.MISC, AccessoryType.RING, AccessoryType.EXTRA_RING, AccessoryType.GLOVES,
-        AccessoryType.EXTRA_MISC };
-
     public static ItemStack getModIcon() {
         return GraveStones.hasAether && ItemsAether.golden_feather != null ? new ItemStack(ItemsAether.golden_feather)
             : new ItemStack(Items.feather);
     }
 
     public static ItemStack removeStackFromSlot(EntityPlayer player, int slotID) {
+        AccessoryType[] slotTypes = new AccessoryType[] { AccessoryType.PENDANT, AccessoryType.CAPE,
+            AccessoryType.SHIELD, AccessoryType.MISC, AccessoryType.RING, AccessoryType.EXTRA_RING,
+            AccessoryType.GLOVES, AccessoryType.EXTRA_MISC };
         if (slotID < slotTypes.length) {
             IPlayerAether playerAether = AetherAPI.get(player);
             IAccessoryInventory accessoryInventory = playerAether.getAccessoryInventory();
