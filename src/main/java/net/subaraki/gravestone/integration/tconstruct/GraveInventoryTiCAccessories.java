6424bc07aa3c2b804a037b8c81a2ffebe099fec6
Gordon-Frohman <sanisimov6699@gmail.com>
2026-05-11 00:25:46 +0700

    Fixed TConstruct accessories synchronization

--- src/main/java/net/subaraki/gravestone/integration/tconstruct/GraveInventoryTiCAccessories.java
+++ src/main/java/net/subaraki/gravestone/integration/tconstruct/GraveInventoryTiCAccessories.java
@@ -1,8 +1,14 @@
 package net.subaraki.gravestone.integration.tconstruct;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.inventory.IInventory;
+import net.minecraft.nbt.NBTTagCompound;
 import net.subaraki.gravestone.tileentity.TileEntityGravestone;
+
+import tconstruct.TConstruct;
+import tconstruct.armor.player.TPlayerStats;
+import tconstruct.util.network.ArmourGuiSyncPacket;
 
 public class GraveInventoryTiCAccessories extends GraveInventoryTiC {
 
@@ -12,6 +18,16 @@ public class GraveInventoryTiCAccessories extends GraveInventoryTiC {
 
     public GraveInventoryTiCAccessories(int size, TileEntityGravestone grave) {
         super(TinkersConstructIntegration.inventoryAccessories, size, grave);
+    }
+
+    @Override
+    public void autoEquipItems(EntityPlayer player) {
+        super.autoEquipItems(player);
+        TPlayerStats stats = TPlayerStats.get(player);
+        NBTTagCompound tag = new NBTTagCompound();
+        stats.saveNBTData(tag);
+        ArmourGuiSyncPacket syncPacket = new ArmourGuiSyncPacket(tag);
+        TConstruct.packetPipeline.sendTo(syncPacket, (EntityPlayerMP) player);
     }
 
     @Override
--- src/main/java/net/subaraki/gravestone/integration/tconstruct/TinkersConstructIntegration.java
+++ src/main/java/net/subaraki/gravestone/integration/tconstruct/TinkersConstructIntegration.java
@@ -1,16 +1,20 @@
 package net.subaraki.gravestone.integration.tconstruct;
 
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
 import net.minecraft.init.Items;
 import net.minecraft.inventory.IInventory;
 import net.minecraft.item.ItemStack;
+import net.minecraft.nbt.NBTTagCompound;
 import net.subaraki.gravestone.GraveStones;
 import net.subaraki.gravestone.integration.ModIntegration;
 import net.subaraki.gravestone.inventory.GraveInventory;
 import net.subaraki.gravestone.tileentity.TileEntityGravestone;
 
+import tconstruct.TConstruct;
 import tconstruct.armor.TinkerArmor;
 import tconstruct.armor.player.TPlayerStats;
+import tconstruct.util.network.ArmourGuiSyncPacket;
 
 public class TinkersConstructIntegration extends ModIntegration {
 
@@ -62,6 +66,11 @@ public class TinkersConstructIntegration extends ModIntegration {
                 inv.setInventorySlotContents(i, null);
             }
             if (!graveInv.isEmpty()) {
+                TPlayerStats stats = TPlayerStats.get(player);
+                NBTTagCompound tag = new NBTTagCompound();
+                stats.saveNBTData(tag);
+                ArmourGuiSyncPacket syncPacket = new ArmourGuiSyncPacket(tag);
+                TConstruct.packetPipeline.sendTo(syncPacket, (EntityPlayerMP) player);
                 graveInv.icon = this.getModIcon1();
                 te.inventories.add(graveInv);
                 result = true;
